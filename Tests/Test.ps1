8dd73b648f5bab1338060512a2ff9f55f16d0bf6
Stig Dahl <stig@charlottendal.net>
2020-02-27 23:25:12 +0100

    Removed extra variables

--- Tests/Test.ps1
+++ Tests/Test.ps1
@@ -7,34 +7,34 @@ Function Private:Copy-TestModule {
     [cmdletbinding()]
     param()
 
+    # Find out paths for the module
     $ModuleHome = Split-Path -Path $PSScriptRoot -Parent
-    $ModuleName =  Split-Path -Path $ModuleHome -Leaf
-    $ModulePath = Join-Path -Path $ModuleHome -ChildPath $ModuleName
-    
-    $ProjectFile = Join-Path -Path $ModuleHome -ChildPath $ModuleName -AdditionalChildPath "$ModuleName.psd1" 
-    $ProjectName =  Get-ProjectName -Path $ProjectFile
-    $ProjectVersion =  Get-Metadata -Path $ProjectFile
-    
-    $Destination = @{}
+    $ProjectManifest = Get-PSModuleManifest -Path $ModuleHome
+    $ProjectName =  Get-ProjectName -Path $ProjectManifest
+    $ProjectVersion =  Get-Metadata -Path $ProjectManifest
+
+    # Source and destination for testing the module
+    $ModuleSource = Join-Path -Path $ModuleHome -ChildPath $ProjectName
+    $ModuleDestination = @{}
 
     $Platform = $PSVersionTable.Platform
     Switch ($Platform) {
-        "Unix" {$Destination.Add( "LinuxPath", "${HOME}/.local/share/powershell/Modules/$ProjectName/$ProjectVersion")}
-        "Win32NT" {$Destination.Add("WindowsPath", "$env:HomePATH/Documents/PowerShell/Modules/$ProjectName/$ProjectVersion")}
+        "Unix" {$ModuleDestination.Add( "LinuxPath", "${HOME}/.local/share/powershell/Modules/$ProjectName/$ProjectVersion")}
+        "Win32NT" {$ModuleDestination.Add("WindowsPath", "$env:HomePATH/Documents/PowerShell/Modules/$ProjectName/$ProjectVersion")}
     }
 
-    foreach($DestPath in $Destination.GetEnumerator()) {
+    foreach($DestPath in $ModuleDestination.GetEnumerator()) {
         New-Item $DestPath.value -Force -ErrorAction Ignore
         Remove-Item -Recurse -Force $DestPath.value
-        Copy-Item -Recurse $ModulePath $DestPath.value -Force
+        Copy-Item -Recurse $ModuleSource $DestPath.value -Force
         Get-ChildItem $DestPath.value
     }
-    Write-Verbose "Home: $ModuleHome"
-    Write-Verbose "Name: $ModuleName"
-    Write-Verbose "Path: $ModulePath"
-    Write-Verbose "Project File: $ProjectFile"
+
+    Write-Verbose "ModuleHome: $ModuleHome"
+    Write-Verbose "ModuleSource: $ModuleSource"
+    Write-Verbose "Project File: $ProjectManifest"
     Write-Verbose "Project name and Version: $ProjectName $ProjectVersion"
-    Write-Verbose "Destination: $($destination.values)"
+    Write-Verbose "ModuleDestination: $($ModuleDestination.values)"
 }
 
 Copy-TestModule

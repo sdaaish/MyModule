e24fc04ea9afefd9524f3c08ab7ebe982b1f261d
Stig Dahl <stig@charlottendal.net>
2020-02-27 13:52:27 +0100

    Add admin tests

--- MyModule/Public/Install-MyFonts.ps1
+++ MyModule/Public/Install-MyFonts.ps1
@@ -1,23 +1,47 @@
-# Install preferred fonts with scoop
-scoop bucket add nerd-fonts
-scoop install sudo
-$fonts = @(
-    "AnonymousPro-NF",
-    "Cascadia-Code",
-    "Cascadia-Mono",
-    "CodeNewRoman-NF",
-    "DejaVuSansMono-NF",
-    "Delugia-Nerd-Font",
-    "FiraCode-NF",
-    "FiraMono-NF",
-    "Inconsolata-NF",
-    "RobotoMono-NF",
-    "SourceCodePro-NF",
-    "Terminus-NF",
-    "Ubuntu-NF"
-)
+Function Install-MyFonts {
 
-foreach($font in $fonts){
-    Write-Host "Installing $font"
-    sudo scoop install --global $font
+    [cmdletbinding()]
+    param()
+
+    # Check for Scoop.sh
+    try {
+        Get-Command scoop
+    }
+    catch {
+
+        throw "Need to install scoop first, exiting."
+    }
+
+    # Need to run as administrator
+    try {
+        Test-Administrator
+    }
+    catch {
+        throw "Need to run this as administrator"
+    }
+    finally {
+        # Install preferred fonts with scoop
+        scoop bucket add nerd-fonts
+        scoop install sudo
+        $fonts = @(
+            "AnonymousPro-NF",
+            "Cascadia-Code",
+            "Cascadia-Mono",
+            "CodeNewRoman-NF",
+            "DejaVuSansMono-NF",
+            "Delugia-Nerd-Font",
+            "FiraCode-NF",
+            "FiraMono-NF",
+            "Inconsolata-NF",
+            "RobotoMono-NF",
+            "SourceCodePro-NF",
+            "Terminus-NF",
+            "Ubuntu-NF"
+        )
+
+        foreach($font in $fonts){
+            Write-Host "Installing $font"
+            sudo scoop install --global $font
+        }
+    }
 }

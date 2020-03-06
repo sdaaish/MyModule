5df58ba83c6e99c50c3b97218041e90c641b390e
Stig Dahl <stig@charlottendal.net>
2020-03-06 14:30:49 +0100

    Add function to save to custom location

---
+++ MyModule/Public/Save-ModuleToLocal.ps1
@@ -0,0 +1,25 @@
+Function Save-ModuleToLocal {
+    [cmdletbinding()]
+    Param (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [string]$Name        
+    )
+
+    begin{
+        if ($isLinux){
+            $ModulePath = Resolve-Path "~/.local/share/powershell/Modules"
+        }
+        else {
+            # Check wich version of Powershell
+            switch ($PSVersionTable.PSEdition){
+                "Core" {$version = "PowerShell/Modules"}
+                "Desktop" { $version = "WindowsPowerShell/Modules"}
+            }
+            $ModulePath = Join-Path -Path (Resolve-Path "~/.local") -ChildPath $version
+        }
+    }
+    
+    process {
+        Save-Module -Name $Name -Path $ModulePath
+    }
+}

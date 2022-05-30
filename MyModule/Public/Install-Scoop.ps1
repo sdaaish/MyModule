56f64c442824945e16185f728c4a8ededc759283
Stig Dahl <stig@charlottendal.net>
2022-05-30 19:06:45 +0200

    Update instalation of scoop and winget

--- MyModule/Public/Install-Scoop.ps1
+++ MyModule/Public/Install-Scoop.ps1
@@ -1,8 +1,13 @@
 # Install scoop.sh
-# Works in Powershell 6
+# Works in Powershell 6 and later
 Function Install-Scoop {
     [cmdletbinding()]
     param()
-    try {Invoke-Expression (New-Object net.webclient).downloadstring('https://get.scoop.sh')}
-    catch {}
+    try {
+	Invoke-Expression (New-Object net.webclient).downloadstring('https://get.scoop.sh') -ErrorAction Stop
+    }
+    catch {
+	Invoke-Expression "& {$(irm get.scoop.sh)} -RunAsAdmin"
+    }
+}
 }
--- MyModule/Public/Install-WinGet.ps1
+++ MyModule/Public/Install-WinGet.ps1
@@ -11,18 +11,27 @@ Function Install-WinGet {
     $version = Invoke-RestMethod "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
     $winget = ($version.assets|
       Where-Object Name -eq "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle").browser_download_url
-    $download = Join-Path $(Resolve-Path ~\Downloads) "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-    (New-Object System.Net.WebClient).DownloadFile($winget, $download)
+    $wingetfile = Join-Path $(Resolve-Path ~\Downloads) "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+    (New-Object System.Net.WebClient).DownloadFile($winget, $wingetfile)
 
-    # Dependency
+    # Dependency of VCLibs
     $vclibs ="https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx"
     $vclibsfile = Join-Path $(Resolve-Path ~\Downloads) "Microsoft.VCLibs.x64.14.00.Desktop.app"
     (New-Object System.Net.WebClient).DownloadFile($vclibs, $vclibsfile)
+
+    # Dependency of Microsoft.UI.Xaml.
+    $xaml = "https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.7.1"
+    $xamlfile = Join-Path $(Resolve-Path ~\Downloads) "Microsoft.UI.Xaml.2.7.zip"
+    (New-Object System.Net.WebClient).DownloadFile($xaml, $xamlfile)
+    Expand-Archive -Path $xamlfile -DestinationPath ~/Downloads/Microsoft.UI.Xaml.2.7
+    Add-AppxPackage ~\Downloads\Microsoft.UI.Xaml.2.7\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx
+
     Add-AppxPackage $vclibsfile
-    Add-AppxPackage $download
+    Add-AppxPackage $wingetfile
 
     Remove-Item $vclibsfile -Force
-    Remove-Item $download -Force
+    Remove-Item $wingetfile -Force
+    Remove-Item $xamlfile -Force
 
     $time = $((Get-Date).subtract($start_time).seconds)
     Write-Output "Downloaded WinGet in $time seconds"
